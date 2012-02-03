ecac838c92950a9267717d606bf2092cab46bc75
ibnHatab <lib.aca55a@gmail.com>
2012-02-03 12:31:21 +0200

    move to org-publish mode

---
+++ org-publish.el
@@ -0,0 +1,24 @@
+
+(require 'org-publish)
+
+(setq org-publish-project-alist
+      '(("orgfiles"
+	 :base-directory "org"
+	 :base-extension "org"
+	 :publishing-directory "."
+	 :publishing-function org-publish-org-to-html
+	 :recursive t
+
+	 :headline-levels 3
+	 :section-numbers nil
+	 :table-of-contents nil
+	 :style "<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\"/>"
+	 :org-export-html-style "<link rel=\"stylesheet\" href=\"css/style.css\" type=\"text/css\"/>"
+
+	 :auto-sitemap    t
+	 :sitemap-filename "sitemap.org"
+	 :sitemap-title    "Sitemap"
+
+	 :html-preamble t)
+
+	("website" :components ("orgfiles"))))
